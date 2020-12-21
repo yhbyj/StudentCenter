08658bd234f3c5a62843c5141dd9e8d259ca1403
zjdyez <lizhaijixiao@sina.com>
2020-12-21 20:36:20 +0800

    Basic view now returns minimal HTML

--- records/tests.py
+++ records/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from records.views import home_page
@@ -9,3 +10,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>成长记录</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- records/views.py
+++ records/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>成长记录</title></html>')
