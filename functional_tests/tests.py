e7c606f1325a66a243312982cff796ff979c6781
zjdyez <lizhaijixiao@sina.com>
2020-12-24 09:59:47 +0800

    FT: staging server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 from unittest import skip
 
@@ -15,6 +16,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self) -> None:
         self.browser.quit()
