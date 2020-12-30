b213ff1752431690134e68a1843fb1e52f7eaa4f
zjdyez <lizhaijixiao@sina.com>
2020-12-30 10:18:54 +0800

    Refactor model test: splitted into pack and record respectively

--- records/tests/__init__.py
+++ records/tests/__init__.py
@@ -2,7 +2,7 @@ from .test_views import (
     HomePageTest, NewPackTest, NewRecordTest, PackViewTest,
 )
 from .test_models import (
-    PackAndRecordModelTest,
+    RecordModelTest, PackModelTest,
 )
 from .test_forms import (
     RecordFormTest,
--- records/tests/test_models.py
+++ records/tests/test_models.py
@@ -5,30 +5,18 @@ from django.test import TestCase
 from records.models import Pack, Record
 
 
-class PackAndRecordModelTest(TestCase):
+class RecordModelTest(TestCase):
 
-    def test_saving_and_retrieving_records(self):
-        pack = Pack()
-        pack.save()
-        first_record = Record()
-        first_record.text = '第一条记录'
-        first_record.pack = pack
-        first_record.save()
+    def test_default_text(self):
+        record = Record()
+        self.assertEqual(record.text, '')
 
-        second_record = Record()
-        second_record.text = '第二条记录'
-        second_record.pack = pack
-        second_record.save()
-
-        saved_records = Record.objects.all()
-        self.assertEqual(saved_records.count(), 2)
-
-        first_saved_record = saved_records[0]
-        second_saved_record = saved_records[1]
-        self.assertEqual(first_saved_record.text, '第一条记录')
-        self.assertEqual(first_saved_record.pack, pack)
-        self.assertEqual(second_saved_record.text, '第二条记录')
-        self.assertEqual(second_saved_record.pack, pack)
+    def test_record_is_related_to_pack(self):
+        pack = Pack.objects.create()
+        record = Record()
+        record.pack = pack
+        record.save()
+        self.assertIn(record, pack.record_set.all())
 
     def test_cannot_save_empty_records(self):
         pack = Pack.objects.create()
@@ -36,13 +24,6 @@ class PackAndRecordModelTest(TestCase):
         with self.assertRaises(ValidationError):
             record.save()
             record.full_clean()
-
-    def test_get_absolute_url(self):
-        pack = Pack.objects.create()
-        self.assertEqual(
-            pack.get_absolute_url(),
-            f'/packs/{pack.id}/'
-        )
 
     def test_cannot_save_duplicate_records(self):
         pack = Pack.objects.create()
@@ -77,3 +58,13 @@ class PackAndRecordModelTest(TestCase):
     def test_string_representation(self):
         record = Record(text='成长记录')
         self.assertEqual(str(record), '成长记录')
+
+
+class PackModelTest(TestCase):
+
+    def test_get_absolute_url(self):
+        pack = Pack.objects.create()
+        self.assertEqual(
+            pack.get_absolute_url(),
+            f'/packs/{pack.id}/'
+        )
